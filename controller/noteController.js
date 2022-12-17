686a04321ecf99b2a02c8fabdd34b13582a8e997
mohamed-eltwahy <mohammed.s.eltwahy@gmail.com>
2022-12-17 16:18:49 +0200

    finish crud methods

--- controller/noteController.js
+++ controller/noteController.js
@@ -3,11 +3,11 @@ var memoryStorage = require('../util/memory_storage');
 var noteModel = require('../model/note_model');
 
 exports.getAllNotes = (req, res) => {
-    let DB =  memoryStorage.getValues(memoryStorage.storage)
+    let DB = memoryStorage.getValues(memoryStorage.storage)
     console.log('DB==> ' + DB);
     // let notes = DB.map((x)=>x)
 
-    return res.status(200).send({data:DB});
+    return res.status(200).send({ data: DB });
 
 }
 exports.SaveNote = (req, res) => {
@@ -23,13 +23,50 @@ exports.SaveNote = (req, res) => {
         var Note = noteModel.Note;
         var noteObjjj = new Note(seqId, title, content, createdby, createdAt);
         memoryStorage.storage.setItem(seqId, noteObjjj);
-        return res.status(200 || 201).send({ message: 'save note successfully', note: { title, content } });
+        return res.status(200 || 201).send({ message: 'save note successfully', note: { seqId, title, content } });
     }
 
 }
 exports.UpdateNote = (req, res) => {
-    res.send('update note controller');
+    var createdby = 'admin';
+    var createdAt = new Date();
+    var noteId = req.body.noteId;
+    var title = req.body.title;
+    var content = req.body.content;
+    console.log(req.body, '==BODY')
+    if (!noteId) {
+        return res.status(409).send({ error: 'note Id should be required !' });
+    }
+
+
+    if (!title || !content) {
+        return res.status(409).send({ error: 'title or content should be required !' });
+    } else {
+
+        var noteitem = memoryStorage.storage.getItem(noteId);
+        if (!noteitem) {
+            return res.status(403).send({ error: 'note Id not found' });
+        }
+
+
+        var Note = noteModel.Note;
+        var noteObjjj = new Note(noteId, title, content, createdby, createdAt);
+        memoryStorage.storage.setItem(noteId, noteObjjj);
+        return res.status(200 || 201).send({ message: 'save note successfully', note: { title, content } });
+    }
 }
 exports.deleteNote = (req, res) => {
-    res.send('delete note controller');
+    var noteId = req.params.noteId;
+    if (!noteId) {
+        return res.status(409).send({ error: 'note Id should be required !' });
+    }
+    var noteitem = memoryStorage.storage.getItem(noteId);
+    if (!noteitem) {
+        return res.status(403).send({ error: 'note Id not found' });
+    }
+
+    memoryStorage.storage.removeItem(noteId);
+    return res.status(200 || 201).send({ message: 'delete successfully'});
+
+
 }
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -5,6 +5,6 @@ var router = express.Router();
 router.get('/notes',noteController.getAllNotes)
 router.post('/notes/save',noteController.SaveNote)
 router.put('/notes/update',noteController.UpdateNote)
-router.delete('/notes/delete',noteController.deleteNote)
+router.delete('/notes/delete/:noteId',noteController.deleteNote)
 
 module.exports = router
